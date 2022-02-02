b0d624c7e913b20384280463b4fa1038f845c4f5
trystan <wong19@up.edu>
2022-02-01 17:35:30 -0800

    projectB

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.teamproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView largeView = findViewById(R.id.largeViewCenter);
+        ImageView smallImage = findViewById(R.id.playerFlightCenter);
+        View.OnClickListener displayListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("hi");
+                if(largeView.getVisibility()==View.INVISIBLE){
+                    largeView.setVisibility(View.VISIBLE);
+                }
+                else if(largeView.getVisibility()==View.VISIBLE){
+                    largeView.setVisibility(View.INVISIBLE);
+                }
+            }
+        };
+        smallImage.setOnClickListener(displayListener);
+        displayListener.onClick(smallImage);
     }
 }
