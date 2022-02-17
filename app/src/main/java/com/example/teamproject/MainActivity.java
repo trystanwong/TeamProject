644a53822bdef3f7af330cecd26713d5ab18160c
trystan <wong19@up.edu>
2022-02-17 11:35:45 -0800

    projectC

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -12,22 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ImageView largeView = findViewById(R.id.largeViewCenter);
-        ImageView smallImage = findViewById(R.id.playerFlightCenter);
-        View.OnClickListener displayListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("hi");
-                if(largeView.getVisibility()==View.INVISIBLE){
-                    largeView.setVisibility(View.VISIBLE);
-                }
-                else if(largeView.getVisibility()==View.VISIBLE){
-                    largeView.setVisibility(View.INVISIBLE);
-                }
-            }
-        };
-        smallImage.setOnClickListener(displayListener);
-        displayListener.onClick(smallImage);
     }
 }
