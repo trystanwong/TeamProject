67b4f5d2ed44fd7954ced944d649770923533997
trystan <wong19@up.edu>
2022-02-23 17:50:58 -0800

    project c (toString not finished)

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<Card> cards = new ArrayList<>();
+        for(int i = 0; i < 24; i++){
+            cards.add(new Card("Red Dragon",i));
+        }
+
+
+        GameState gs = new GameState(cards);
+        System.out.println(gs);
     }
 }
